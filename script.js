0ac9ed57484dc0241661089750524e9e60208996
UtovarIstovar <52053123+UtovarIstovar@users.noreply.github.com>
2020-09-18 00:31:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 var zivotinje = ["tigar", "macka", "madjed", "ptica"];
+var dugme = document.getElementById("blabla");
+var spisak = document.getElementById("spisak");
+var i = 1;
 
 console.log("Ovo je najjaci sajt na svetu");
+
+
+function dodaj() {
+
+	var adresa = document.createElement("a");
+	adresa.href = "/";
+	adresa.onclick = obrisi;
+	var hajedan = document.createElement("h1");
+	hajedan.appendChild(document.createTextNode("Stavka " + i.toString()));
+	adresa.appendChild(hajedan);
+	spisak.appendChild(adresa);
+
+	i++;
+}
+
+function obrisi(event) {
+	event.target.remove();
+}
